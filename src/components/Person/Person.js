e8892841cca1e0f11c4cf09c3d32fd658fbb3a35
tashiad <tashiadavis10@gmail.com>
2021-02-01 21:04:03 -0700

    Create Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+function Person(props) {
+  return (
+    props.staff.map(member => {
+      return (
+        <div className="staff" key={member.id}>
+          <img src={member.photo} className="staffImg" alt=''/>
+          <h3>{member.name}</h3>
+          <p className="quote">{member.quote}</p>
+          <p className="superlative">{member.superlative}</p>
+        </div>
+      )
+    })
+  )
+}
 
 export default Person;
